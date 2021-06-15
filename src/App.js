a9f85e82de9d46d69d65f82eb0b849452fa1dc93
iulianstan <iulian1226@gmail.com>
2021-06-15 15:02:26 +0200

    src/Projects.js/ Created Projects page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from "./Home";
 import About from "./About";
+import Projects from "./Projects";
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
+      <Projects />
     </div>
   );
 }
