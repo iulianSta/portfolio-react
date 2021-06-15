278b15877e1d8d728a76a5294db6c0ddca631cba
iulianstan <iulian1226@gmail.com>
2021-06-15 15:05:23 +0200

    src/Contact.js/ Created Contact page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from "./Home";
 import About from "./About";
 import Projects from "./Projects";
+import Contact from "./Contact";
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
       <Projects />
+      <Contact />
     </div>
   );
 }
