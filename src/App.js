d58f29285100b9b2b01c68d8e4d5c25e566aa461
iulianstan <iulian1226@gmail.com>
2021-06-15 14:55:30 +0200

    src/Home.js/ Created Home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import Home from "./Home";
 function App() {
   return (
     <div className="App">
-      <h1> App Test...</h1>
-      <p>Nothing to be showed for the moment</p>
+      <Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <h3>App Test...</h3>
+      <p>Nothing to be showed for the moment</p>
+    </div>
+  );
+};
+
+export default Home;
