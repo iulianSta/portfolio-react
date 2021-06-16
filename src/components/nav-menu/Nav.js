608949f8aca2d82fea39e4688a2a9e9e95a043fa
iulianstan <iulian1226@gmail.com>
2021-06-16 11:00:28 +0200

    src/App.js, components/ Modify menu

--- src/components/nav-menu/Nav.js
+++ src/components/nav-menu/Nav.js
@@ -1,11 +1,26 @@
+// Import area
+import React from "react";
+import { Link } from "react-router-dom";
+
 // Nav function
 const Nav = () => {
   // Navigation menu
-  const menu = ["Home", "About me", "Projects", "Contact"];
-
-  const navBar = menu.map((el, i) => <li key={i}>{el}</li>);
-
-  return <ul>{navBar}</ul>;
+  return (
+    <nav>
+      <Link to="/">
+        <li>Home</li>
+      </Link>
+      <Link to="/about">
+        <li>About me</li>
+      </Link>
+      <Link to="/projects">
+        <li>Projects</li>
+      </Link>
+      <Link to="/contact">
+        <li>Contact</li>
+      </Link>
+    </nav>
+  );
 };
 
 // Nav export
