6798f4dd568c8ce520685ab69af5fee616477912
iulianstan <iulian1226@gmail.com>
2021-06-16 14:51:41 +0200

    src/components/About.js/ Content added

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,29 @@
 const About = () => {
   return (
     <div>
-      <h1>About page</h1>
+      <h1>About me...</h1>
+      <p>
+        After 10 years of military career in the Romanian Army
+        <br />
+        I decided to change my lifestyle, moving with my family to
+        <br />
+        Germany. I've allways wanted to learn to code, so I decided to give
+        <br />
+        myself a chance to change my career. I’m currently a student
+        <br />
+        and I'm learning web development at{" "}
+        <a href="http://www.digitalcareerinstitute.org" target="blank">
+          <strong>DCI Digital Career Institute</strong>
+        </a>
+        .
+      </p>
+      <p>
+        <span>Some of my hobbies are:</span>
+        <br />
+        coding, music, photography, cars, fishing,traveling,
+        <br />
+        playing entertainment poker and video games.
+      </p>
     </div>
   );
 };
