65ce4c122557c06f7c1cfb48fbc1130e36f94399
iulianstan <iulian1226@gmail.com>
2021-06-16 15:03:42 +0200

    src/components/Contact.js/ Contact content added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,12 @@
 const Contact = () => {
   return (
     <div>
-      <h1>Contact Page</h1>
+      <h1>Contact me...</h1>
+      <p>
+        If you have any questions or if you want to
+        <br />
+        collaborate with me, you can send me a message
+      </p>
     </div>
   );
 };
