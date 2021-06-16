3aab3b52b2d0220046be7f49c740d0aabf604a87
iulianstan <iulian1226@gmail.com>
2021-06-16 09:21:00 +0200

    src/index.js/ Imported icons library and added comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+// Import area
 import React from "react";
 import ReactDOM from "react-dom";
+import "./components/icons/lib";
 import "./sass/main.scss";
 import App from "./App";
 
+// ReactDOM render
 ReactDOM.render(
   <React.StrictMode>
     <App />
