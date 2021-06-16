b066744e3a323fdd88e240be2d245567d24f01c2
iulianstan <iulian1226@gmail.com>
2021-06-16 09:18:04 +0200

    src/components/home-components/Main.js/ Separated my paragraph in two

--- src/components/home-components/Main.js
+++ src/components/home-components/Main.js
@@ -2,7 +2,8 @@ const Main = () => {
   return (
     <div>
       <h3>Hi, I'm Iulian Stan</h3>
-      <p>Former military policeman Current web developer student</p>
+      <p>Former military policeman</p>
+      <p>Current web developer student</p>
     </div>
   );
 };
