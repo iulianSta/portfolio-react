726bfe0c5d3d02e11d9d832a57402e0bf7da29ac
iulianstan <iulian1226@gmail.com>
2021-06-17 13:49:14 +0200

    src/components/Projects.js/ Adding content for project cards build

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,10 @@ const Projects = () => {
   return (
     <div className="projects">
       <h1>My projects</h1>
+      <div className="cards">
+        <div className="card">CARD</div>
+        <div className="card">CARD</div>
+      </div>
     </div>
   );
 };
