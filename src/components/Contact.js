0de2319c83297e3688a255f0947a07b9d00b27f8
iulianstan <iulian1226@gmail.com>
2021-06-17 14:20:23 +0200

    src/ Contact setup and adding content

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,9 @@ const Contact = () => {
       <p className="c-para">
         If you have any questions or if you want to
         <br />
-        collaborate with me, you can send me a message
+        collaborate with me, you can send me a message to
+        <br />
+        <strong>iulianstangithub@gmail.com</strong>
       </p>
     </div>
   );
