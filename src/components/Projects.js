5b2aa1faa84705c05e59453e806be875c8f0c6cc
iulianstan <iulian1226@gmail.com>
2021-06-21 23:02:25 +0200

    Imported informations from json file and adding style setup to projects.scss

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,8 +10,8 @@ const Projects = () => {
   return (
     <div className="projects">
       <h1>My projects...</h1>
-      <h3>After I learned the basics of:</h3>
-      <div>{addSkillz}</div>
+      <h3>After I learned:</h3>
+      <div className="logo-container">{addSkillz}</div>
       <h3>I was able to do the following projects:</h3>
       <div className="cards">
         <div className="card">CARD</div>
