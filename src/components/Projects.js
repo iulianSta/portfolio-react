8dc0074e625e22f1761e46aacd8342e56eb61195
iulianstan <iulian1226@gmail.com>
2021-06-21 15:31:36 +0200

    Adding info's to Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,10 @@
 const Projects = () => {
   return (
     <div className="projects">
-      <h1>My projects</h1>
+      <h1>My projects...</h1>
+      <h3>After I learned the basics of:</h3>
+      <div>Logo's</div>
+      <h3>I was able to do the following projects:</h3>
       <div className="cards">
         <div className="card">CARD</div>
         <div className="card">CARD</div>
