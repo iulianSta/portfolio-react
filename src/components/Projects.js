4cc0450f5870bb8d6cfdaee1ae29470b9e2329f9
iulianstan <iulian1226@gmail.com>
2021-06-21 16:06:56 +0200

    Projects.js/ Importing and adding skills logo from json fine

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,17 @@
+// Import area
+import Skillz from "../projects.json";
+
 // Projects function
 const Projects = () => {
+  const addSkillz = Skillz.map((obj) => {
+    const { id, skill, cardImg } = obj;
+    return <img key={id} className="skill-logo" src={skill} alt="Linux Logo" />;
+  });
   return (
     <div className="projects">
       <h1>My projects...</h1>
       <h3>After I learned the basics of:</h3>
-      <div>Logo's</div>
+      <div>{addSkillz}</div>
       <h3>I was able to do the following projects:</h3>
       <div className="cards">
         <div className="card">CARD</div>
