f631dcb3790dfeb0b9f658810f964d4e3f89e1a7
iulianstan <iulian1226@gmail.com>
2021-06-22 01:16:17 +0200

    Imported informations from json file and adding style setup to projects.scss

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,22 +1,32 @@
 // Import area
 import Skillz from "../projects.json";
-
+import Images from "../images.json";
 // Projects function
 const Projects = () => {
   const addSkillz = Skillz.map((obj) => {
-    const { id, skill, cardImg } = obj;
-    return <img key={id} className="skill-logo" src={skill} alt="Linux Logo" />;
+    const { id, skill } = obj;
+    return (
+      <img key={id} className="skill-logo" src={skill} alt="Project Logo" />
+    );
   });
+
+  const insertImages = Images.map((obj) => {
+    const { id, img } = obj;
+    return <img key={id} className="project-img" src={img} alt="Projects" />;
+  });
+
   return (
-    <div className="projects">
+    <div id="top" className="projects">
       <h1>My projects...</h1>
       <h3>After I learned:</h3>
       <div className="logo-container">{addSkillz}</div>
       <h3>I was able to do the following projects:</h3>
       <div className="cards">
-        <div className="card">CARD</div>
-        <div className="card">CARD</div>
+        <div className="card">{insertImages}</div>
       </div>
+      <button className="gotop">
+        <a href="#top">⬆️ Top</a>
+      </button>
     </div>
   );
 };
