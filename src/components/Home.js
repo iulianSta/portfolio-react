c56764be751d734f76e3f81ec2c8d4d418507f86
iulianstan <iulian1226@gmail.com>
2021-06-22 03:31:04 +0200

    Adding touches to home.scss on mobile layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,29 @@
+// Import area
+import Logo from "../logo.json";
+
 // Home funcrion
 const Home = () => {
+  const getImage = Logo.map((obj) => {
+    const { id, image } = obj;
+    return (
+      <img
+        key={id}
+        className="profile-img"
+        src={image}
+        alt="Iulian Stan Draw"
+      />
+    );
+  });
+
   return (
     <div className="home">
-      <h1 className="h-header">Hi, I'm Iulian Stan</h1>
-      <p className="h-para">Former military policeman</p>
-      <p className="h-para">Current web developer student</p>
+      <h1 className="h-header">
+        Hi,
+        <br /> I'm Iulian Stan
+      </h1>
+      <p className="h-para">Former Military Policeman</p>
+      <p className="h-para">Current Web Developer</p>
+      <div>{getImage}</div>
     </div>
   );
 };
