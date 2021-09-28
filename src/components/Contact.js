21f14e7292da044a1cda6e767def27531d0b5383
iulianstan <iulian1226@gmail.com>
2021-09-28 14:51:06 +0200

    Form info fixed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,18 @@
 // Import area
-//import React, { useState } from "react";
+import React, { useState } from "react";
 
 // Contact function
 const Contact = () => {
-  // const [userInput, setUserInput] = useState("");
+  const [userInput, setUserInput] = useState("");
 
-  // function changeHandle(e) {
-  //   setUserInput(e.target.value);
-  // }
+  function changeHandle(e) {
+    setUserInput(e.target.value);
+  }
 
-  // function submitHandle(e) {
-  //   e.preventDefault();
-  //   setUserInput("");
-  // }
+  function submitHandle(e) {
+    e.preventDefault();
+    setUserInput("");
+  }
   return (
     <div className="contact">
       <h1 className="c-header">Contact me...</h1>
@@ -25,24 +25,27 @@ const Contact = () => {
         !
       </p>
       <fieldset>
-        <legend>E-Mail & Location</legend>
+        {/* <legend>E-Mail & Location</legend>
         <strong>iulianstangithub@gmail.com</strong>
-        <p>04600 Altenburg</p>
-        {/* <form onSubmit={submitHandle}>
+        <p>04600 Altenburg</p> */}
+        <legend>E-Mail</legend>
+        <form 
+        action="https://formspree.io/f/xeqvdjro"
+        method="POST">
           <input
             type="text"
-            value={userInput}
-            onChange={changeHandle}
+            name="name"
             placeholder="Name"
           />
-          <input type="email" placeholder="E-Mail" onChange={changeHandle} />
+          <input type="email" name="_replyto" placeholder="E-Mail"/>
           <textarea
             type="text"
+            name="message"
             placeholder="Your message"
-            onChange={changeHandle}
+          
           />
           <button type="submit">Send</button>
-        </form> */}
+        </form>
       </fieldset>
     </div>
   );
